b703fa454c37f35b353bc67b3dfba6cb7ffa0296
AdrianCo97 <adrian.corazza@outlook.com>
2021-12-14 12:54:51 +0100

    Updated some routes

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,10 +1,12 @@
-const express = require("express");
-const Database = require("better-sqlite3");
+import express from "express";
 
+import trainStationRoutes from "./routes/trainstations.js";
 const app = express();
 const PORT = 5000;
 
 app.use(express.json());
+
+app.use("/trainstations", trainStationRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server started at port: ${PORT}`)
