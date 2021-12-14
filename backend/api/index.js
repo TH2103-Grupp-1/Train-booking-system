b5f661fee3ec30574dabc84fd5b16fef3b9354f7
AdrianCo97 <adrian.corazza@outlook.com>
2021-12-14 16:21:55 +0100

    Heightened the limit of body parser

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,10 +1,11 @@
+import bodyParser from "body-parser";
 import express from "express";
 
 import trainStationRoutes from "./routes/trainstations.js";
 const app = express();
 const PORT = 5000;
 
-app.use(express.json());
+app.use(bodyParser.json({limit: "50mb"}));
 
 app.use("/trainstations", trainStationRoutes);
 
