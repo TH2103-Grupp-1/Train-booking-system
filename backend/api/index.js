bdb4d77b7359064746a91b56e9046489e2a41520
AdrianCo97 <adrian.corazza@outlook.com>
2021-12-15 13:52:06 +0100

    Created routes for users

--- backend/api/index.js
+++ backend/api/index.js
@@ -3,6 +3,7 @@ import express from "express";
 
 import trainStationRoutes from "./routes/trainstations.js";
 import bookingRoutes from "./routes/bookings.js";
+import userRoutes from "./routes/users.js";
 const app = express();
 const PORT = 5000;
 
@@ -10,7 +11,10 @@ app.use(bodyParser.json({limit: "50mb"}));
 
 app.use("/trainstations", trainStationRoutes);
 
-app.use("/bookings", bookingRoutes)
+app.use("/bookings", bookingRoutes);
+
+app.use("/users", userRoutes);
+
 app.listen(PORT, () => {
   console.log(`Server started at port: ${PORT}`)
 });
