836d9d153b009323167d9dbcb7826e988877f93d
AdrianCo97 <adrian.corazza@outlook.com>
2021-12-15 12:07:02 +0100

    imported a controller function

--- backend/api/routes/bookings.js
+++ backend/api/routes/bookings.js
@@ -1,7 +1,8 @@
-import express from "express"
+import express from "express";
+import { createBooking } from "../controllers/bookings.js";
 const router = express.Router();
 
-router.post("/");
+router.post("/", createBooking);
 
 router.get("/");
 
