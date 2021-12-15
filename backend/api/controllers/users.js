85749b0ea3a8c8fd6369d1b6639af2a7d9a40a50
AdrianCo97 <adrian.corazza@outlook.com>
2021-12-15 16:41:38 +0100

    Fixed an issue in the encrypt function

--- backend/api/controllers/users.js
+++ backend/api/controllers/users.js
@@ -1,16 +1,36 @@
 import crypto from "crypto";
+import Database from "better-sqlite3";
 
-const algorithm = "osIk1^rcczs-*"
-const secretKey = "LooOLisaISIXCoO02l12idxlcozPOIösoq"
+let db = new Database("../database.db");
+
+const algorithm = "aes-256-cbc"
+const secretKey = "LooOLisaISIXCoO02l12idxlcozPOIso"
 const iv = crypto.randomBytes(16);
 
 const encrypt = (text) => {
   const cipher = crypto.createCipheriv(algorithm, secretKey, iv);
 
-  const encrypted = Buffer.concat([cipher.update(text), cipher.final()]);
+  let encrypted = cipher.update(text, 'utf-8', 'hex');
 
-  return {
-    iv: iv.toString('hex'),
-    content: encrypted.toString('hex')
-  };
+  encrypted += cipher.final('hex');
+
+  return encrypted;
+}
+
+export const createUser = (req, res) => {
+  let username = req.body.username;
+  let firstName = req.body.firstName;
+  let lastName = req.body.lastName;
+  let email = req.body.email;
+  let phoneNumber = req.body.phoneNumber;
+  let password = req.body.password;
+
+  let encryptedPassword = encrypt(password);
+
+  let preparedStatement = db.prepare("INSERT INTO Users (username, firstName, lastName, email, phoneNumber, password) VALUES(?,?,?,?,?,?)");
+
+  preparedStatement.run(username, firstName, lastName, email, phoneNumber, encryptedPassword);
+
+  res.send("Data inserted!")
+
 }
