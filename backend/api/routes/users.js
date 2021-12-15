92820ef0b90915fc6d00bd46c7e7beba9563eddb
AdrianCo97 <adrian.corazza@outlook.com>
2021-12-15 16:42:19 +0100

    Created a route for createUser

--- backend/api/routes/users.js
+++ backend/api/routes/users.js
@@ -1,7 +1,8 @@
 import express from "express";
+import { createUser } from "../controllers/users.js";
 const router = express.Router();
 
-router.post("/");
+router.post("/", createUser);
 
 router.get("/");
 
