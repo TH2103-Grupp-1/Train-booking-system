6d357f8bee840289cb55fcdd11e4c19c9ab112be
AdrianCo97 <adrian.corazza@outlook.com>
2021-12-15 12:40:02 +0100

    Inserted the getBookingById function into the route

--- backend/api/routes/bookings.js
+++ backend/api/routes/bookings.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { createBooking, getAllBookings } from "../controllers/bookings.js";
+import { createBooking, getAllBookings, getBookingById } from "../controllers/bookings.js";
 const router = express.Router();
 
 router.post("/", createBooking);
 
 router.get("/", getAllBookings);
 
-router.get("/:id");
+router.get("/:id", getBookingById);
 
 export default router;
