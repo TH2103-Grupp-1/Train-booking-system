741af568bf4355345c30caae3964976e5cb3006b
AdrianCo97 <adrian.corazza@outlook.com>
2021-12-16 16:21:56 +0100

    Implemented the encrypt function into the createUser function

--- backend/api/controllers/users.js
+++ backend/api/controllers/users.js
@@ -2,7 +2,7 @@ import Database from "better-sqlite3";
 import {encrypt} from "../controllers/crypto.js"
 let db = new Database("../database.db");
 
-export const createUser = (req, res) => {
+export const createUser = async(req, res) => {
   let username = req.body.username;
   let firstName = req.body.firstName;
   let lastName = req.body.lastName;
@@ -10,8 +10,8 @@ export const createUser = (req, res) => {
   let phoneNumber = req.body.phoneNumber;
   let password = req.body.password;
 
-  let encryptedPassword = encrypt(password);
-
+  let encryptedPassword = await encrypt(password);
+  
   let preparedStatement = db.prepare("INSERT INTO Users (username, firstName, lastName, email, phoneNumber, password) VALUES(?,?,?,?,?,?)");
 
   preparedStatement.run(username, firstName, lastName, email, phoneNumber, encryptedPassword);
