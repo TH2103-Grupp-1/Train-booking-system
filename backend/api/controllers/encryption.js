f4e99d60e36b556287f497197544726032ff98b9
AdrianCo97 <adrian.corazza@outlook.com>
2021-12-16 16:18:17 +0100

    Updated the file name

--- backend/api/controllers/encryption.js
+++ backend/api/controllers/encryption.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcryptjs";
 
-export const encrypt = (password) => {
+export const encrypt = async (password) => {
   const salt = await bcrypt.genSalt(10);
   const hashPassword = await bcrypt.hash(password, salt);
   return hashPassword;
