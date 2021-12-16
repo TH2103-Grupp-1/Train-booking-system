a1465d031b03d79dba5058ec507c8c2609266646
AdrianCo97 <adrian.corazza@outlook.com>
2021-12-16 15:48:22 +0100

    Removed the crypto code and instead installed the bcrypt module.

--- backend/api/controllers/crypto.js
+++ backend/api/controllers/crypto.js
@@ -1,23 +1,7 @@
-import crypto from "crypto";
-
-const algorithm = "aes-256-cbc"
-const secretKey = "LooOLisaISIXCoO02l12idxlcozPOIso"
-const iv = crypto.randomBytes(16);
+import bcrypt from "bcryptjs";
 
 export const encrypt = (password) => {
-  const cipher = crypto.createCipheriv(algorithm, secretKey, iv);
-
-  let encrypted = cipher.update(password, 'utf-8', 'hex');
-
-  encrypted += cipher.final('hex');
-
-  return encrypted;
-}
-
-export const decrypt = async (password) => {
-  let decipher = crypto.createDecipheriv('aes-256-cbc', secretKey, iv); 
-  let decrypted = decipher.update(password, 'hex', 'utf-8');
-  decrypted += decipher.final('utf-8');
-
-  return decrypted;
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(password, salt);
+  return hashPassword;
 }
