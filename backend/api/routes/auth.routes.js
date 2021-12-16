071c902024d368975e0ae7e0bbb6967b0a0d5121
AdrianCo97 <adrian.corazza@outlook.com>
2021-12-16 12:49:01 +0100

    Created a function for validating the credentials of a user.

--- backend/api/routes/auth.routes.js
+++ backend/api/routes/auth.routes.js
@@ -1,12 +1,30 @@
 import express from "express";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
+import { decrypt } from "../controllers/crypto.js";
 
 const router = express.Router();
 
 router.post('/login', (req, res) => {
-    const user = {
-        id: 1,
-        email: 'liam@gmail.com'
+
+    let email = req.body.email;
+    let password = req.body.password;
+
+    const preparedStatement = db.prepare("SELECT * FROM Users WHERE email = ?")
+
+    const user = preparedStatement.get(email);
+
+    if (!user) {
+        res.status(400).send("There is no registered user with that email.");
+    }
+    else {
+        let decryptedPassword = decrypt(password);
+
+        if (decryptedPassword === user.password) {
+            res.send("Credentials were successfully validated!");
+        }
+        else {
+            res.send("The password was incorrect.")
+        }
     }
 
     jwt.sign({user}, 'secretkey', { expiresIn: '1h' } ,(err, token) => {
