3201f6ea0d95956eee075ebe8907eaf525d59149
Liam Björkman <liambjorkman00@gmail.com>
2021-12-17 12:00:51 +0100

    Remove duplicate code

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -16,8 +16,6 @@ export class NavbarComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.translate.loadLanguages();
-    this.isLoggedIn();
     let currentLanguage = localStorage.getItem("language");
     if (currentLanguage !== null) {
       this.setLanguage(currentLanguage);
