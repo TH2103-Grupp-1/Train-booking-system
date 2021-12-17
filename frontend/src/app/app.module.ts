62523aca8ecf0ccac2dd6c9ad7af4db10cbc5698
Adnan Kovacevic <69817904+Ado-wq@users.noreply.github.com>
2021-12-17 14:59:15 +0100

    Pagination added
    added pagination to the page, show 4 departures per site

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -11,13 +11,15 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { AuthModule } from './auth/auth.module';
 import { Notyf } from 'notyf';
 import { notyfFactory } from './shared/guards/notyf.token';
 import { DepartureComponent } from './departureSelection/departure/departure.component';
+import { NgxPaginationModule } from 'ngx-pagination';
+
 
 
 
@@ -35,6 +37,7 @@ export function HttpLoaderFactory(http: HttpClient) {
   ],
   imports: [
     BrowserModule,
+    NgxPaginationModule,
     BrowserAnimationsModule,
     AuthModule,
     AppRoutingModule,
--- frontend/src/app/departureSelection/departure/departure.component.ts
+++ frontend/src/app/departureSelection/departure/departure.component.ts
@@ -12,12 +12,38 @@ export class DepartureComponent implements OnInit {
     { "id": 2, "departure": "08:45", "secondClass": "pris", "firstClass": "pris" },
     { "id": 3, "departure": "09:45", "secondClass": "pris", "firstClass": "pris" },
     { "id": 4, "departure": "10:45", "secondClass": "pris", "firstClass": "pris" },
-    { "id": 5, "departure": "11:45", "secondClass": "pris", "firstClass": "pris" }
+    { "id": 5, "departure": "11:45", "secondClass": "pris", "firstClass": "pris" },
+    { "id": 6, "departure": "17:45", "secondClass": "pris", "firstClass": "pris" },
+    { "id": 7, "departure": "18:45", "secondClass": "pris", "firstClass": "pris" },
+    { "id": 8, "departure": "19:45", "secondClass": "pris", "firstClass": "pris" },
+    { "id": 9, "departure": "20:45", "secondClass": "pris", "firstClass": "pris" },
+    { "id": 10, "departure": "11:30", "secondClass": "pris", "firstClass": "pris" },
+    { "id": 11, "departure": "07:30", "secondClass": "pris", "firstClass": "pris" },
+    { "id": 12, "departure": "08:30", "secondClass": "pris", "firstClass": "pris" },
+    { "id": 13, "departure": "09:45", "secondClass": "pris", "firstClass": "pris" },
+    { "id": 14, "departure": "10:20", "secondClass": "pris", "firstClass": "pris" },
+    { "id": 15, "departure": "11:25", "secondClass": "pris", "firstClass": "pris" },
+    { "id": 16, "departure": "07:40", "secondClass": "pris", "firstClass": "pris" },
+    { "id": 17, "departure": "08:30", "secondClass": "pris", "firstClass": "pris" },
+    { "id": 18, "departure": "09:30", "secondClass": "pris", "firstClass": "pris" },
+    { "id": 19, "departure": "10:10", "secondClass": "pris", "firstClass": "pris" },
+    { "id": 20, "departure": "11:15", "secondClass": "pris", "firstClass": "pris" },
   ]
 
   constructor() { }
 
+
+  totalLength: any;
+  page: number = 1;
+
+
+
+
   ngOnInit(): void {
+
+    this.totalLength = this.tableData.length
+    console.log(this.tableData.length);
+    console.log(this.totalLength)
   }
 
 }
