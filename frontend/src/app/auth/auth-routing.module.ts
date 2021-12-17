44fc70050a01e0d4f9c96bd7d98f59f5031e4d1d
Liam Björkman <liambjorkman00@gmail.com>
2021-12-17 14:52:21 +0100

    Account page routed

--- frontend/src/app/auth/auth-routing.module.ts
+++ frontend/src/app/auth/auth-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountPageComponent } from '../account-page/account-page.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: 'account', component: AccountPageComponent }
 ];
 
 @NgModule({
