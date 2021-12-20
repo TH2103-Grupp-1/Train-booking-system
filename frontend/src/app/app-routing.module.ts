fcf41e18b76b49599030c2d413ef87548414b8db
kevinkallin <kevin.kallin@hotmail.se>
2021-12-20 20:22:29 +0100

    Added payment layout

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { StationsComponent } from './stations/stations.component';
 import { AuthGuard } from './shared/guards/auth.guard';
 import { AccountPageComponent } from './account-page/account-page.component';
+import { PaymentComponent } from './booking/payment/payment.component';
 
 
 const routes: Routes = [
   { path: 'stations', component: StationsComponent, canActivate: [AuthGuard]},
   { path: '', component: LandingPageComponent},
-  { path: 'account', component: AccountPageComponent}
+  { path: 'account', component: AccountPageComponent},
+  { path: 'payment', component: PaymentComponent }
 ];
 
 @NgModule({
