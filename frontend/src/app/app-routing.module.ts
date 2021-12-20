f65a47b7302fa3daf688f78e0126c165b02ac7ce
Adnan Kovacevic <69817904+Ado-wq@users.noreply.github.com>
2021-12-20 09:57:44 +0100

    Update app-routing.module.ts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,19 +4,15 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { StationsComponent } from './stations/stations.component';
 import { AuthGuard } from './shared/guards/auth.guard';
 import { DepartureComponent } from './departureSelection/departure/departure.component';
+import { AccountPageComponent } from './account-page/account-page.component';
 
 const routes: Routes = [
   { path: 'stations', component: StationsComponent, canActivate: [AuthGuard] },
   { path: '', component: LandingPageComponent },
   { path: 'departures', component: DepartureComponent },
-import { AccountPageComponent } from './account-page/account-page.component';
-
-
-const routes: Routes = [
-  { path: 'stations', component: StationsComponent, canActivate: [AuthGuard] },
-  { path: '', component: LandingPageComponent },
   { path: 'account', component: AccountPageComponent }
-];
+]
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
