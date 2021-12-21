54f87fe76b100bc89f7d6a9f91562356a21ba8f9
jonaslindell-th <Carljonaslindell@gmail.com>
2021-12-21 15:46:10 +0100

    adds timetable model

---
+++ frontend/src/app/models/timetable.model.ts
@@ -0,0 +1,9 @@
+import { Station } from "./station.model";
+
+export interface TimeTable {
+  Id: number;
+  DepartureStation: Station;
+  DepartureTime: Date;
+  ArrivalStation: Station;
+  ArrivalTime: Date;
+}
