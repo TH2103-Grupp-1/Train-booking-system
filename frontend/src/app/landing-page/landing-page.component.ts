c49ce3f49eebfe05feb04d889b0f8239f9288d10
AdrianCo97 <adrian.corazza@outlook.com>
2021-12-21 15:46:07 +0100

    Created a function for toggling the calendar.

--- frontend/src/app/landing-page/landing-page.component.ts
+++ frontend/src/app/landing-page/landing-page.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent implements OnInit {
+  checked: boolean = false;
+
+  disabledCelandarStyle: string = "opacity: 0.2; pointer-events: none; user-select: none;";
+
+  calendarStyle: string = this.disabledCelandarStyle;
 
   constructor() { }
 
+  onCheck() {
+    if (!this.checked) {
+      this.checked = true;
+
+      this.calendarStyle = "";
+    }
+    else if (this.checked) {
+      this.checked = false;
+
+      this.calendarStyle = this.disabledCelandarStyle;
+    }
+    
+  }
+
   ngOnInit(): void {
   }
+
+  
 
 }
