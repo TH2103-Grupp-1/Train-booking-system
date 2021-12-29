29039160dc41da70c8b4ae81f799ab0888163c00
AdrianCo97 <adrian.corazza@outlook.com>
2021-12-29 14:11:15 +0100

    Created basic get functions for the new tables

---
+++ backend/api/controllers/carriages.js
@@ -0,0 +1,9 @@
+import { db } from "../index.js";
+
+export const getAllTrains = (req, res) => {
+  const statement = db.prepare("SELECT * FROM Carriages");
+
+  const results = statement.all();
+
+  res.send(results);
+}
