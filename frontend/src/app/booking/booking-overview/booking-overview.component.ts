856e8e841a6b77431b7be19abcfacf1f2f9168d5
Adnan Kovacevic <69817904+Ado-wq@users.noreply.github.com>
2022-01-14 10:00:41 +0100

    Revert "a"
    This reverts commit 9fb268e8bdfd6c6d381a7d184e0f33adaa9791dc.

--- frontend/src/app/booking/booking-overview/booking-overview.component.ts
+++ frontend/src/app/booking/booking-overview/booking-overview.component.ts
@@ -18,58 +18,8 @@ export class BookingOverviewComponent implements OnInit {
    }
 
   ngOnInit(): void {
-   
   }
 
-
-  getTravelTime(): string{
-    let ArrivalTime = this.booking.TimeTable?.ArrivalTime?.toString().replace(':', "");
-    let DepartureTime = this.booking.TimeTable?.DepartureTime?.toString().replace(':', "");
-
-   // let departureTime = new Date(DepartureTime?.getHours()!, DepartureTime?.getMinutes()!);
-    //let arrivalTime = new Date(ArrivalTime?.getHours()!, ArrivalTime?.getMinutes()!);
-    let arrTime = Date.parse(ArrivalTime!);
-    let depTime = Date.parse(DepartureTime!);
-
-    //let timeDiff = this.msToHMS(Math.abs(+ArrivalTime! - +DepartureTime!));
-    let msDiff = arrTime! - depTime!;
-    let timeDiff = this.msToHMS(msDiff);
-    
-    //document.getElementById("travelTimePhone")!.innerHTML = timeDiff;
-    //document.getElementById("travelTimeDesktop")!.innerHTML = timeDiff;
-
-    return timeDiff;
-  }
-
-   msToHMS(ms: number): string{
-  
-    let seconds = ms / 1000;
-  
-    let hours = Math.floor(seconds / 3600); 
-  
-    let minutes = Math.floor(seconds - (hours * 3600)) / 60; 
-   
-    let time = hours.toString() + ':' +  minutes.toString();
-
-    return time;
-}
-
-getDepartureDate(): string{
-  let formatedDate = Date.parse(this.booking.TimeTable!.DepartureTime!.toString().replace(':', ""));
-  let date = new Date(formatedDate).toString();
-
-  let matches = /([^-]+):([^-]+):([^-]+)/.exec(date);
-  return matches![1] + ':' + matches![2];
-}
-
-iconTogglePhone(){
-  
-  document.getElementById('iconPhone')?.classList.toggle('fa-caret-up');  
-}
-iconToggleDesktop(){
-  
-  document.getElementById('iconDesktop')?.classList.toggle('fa-caret-up');  
-}
 
   submit() {
     this.route.navigateByUrl('/payment');
